f301ce43f5c2f0aa8a7ce5149cb5ee1377e4cf4c
Edmundo <edmundoach@hotmail.com>
2019-12-14 02:07:27 -0500

    fux

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyDsDCh1_eqm2-zNzTL-4bSz-BiyUKShYU0",
+    authDomain: "login-91954.firebaseapp.com",
+    databaseURL: "https://login-91954.firebaseio.com",
+    projectId: "login-91954",
+    storageBucket: "login-91954.appspot.com",
+    messagingSenderId: "6315715226",
+    appId: "1:6315715226:web:24d79d92c1fa4b63d04d23"
+  }
 };
